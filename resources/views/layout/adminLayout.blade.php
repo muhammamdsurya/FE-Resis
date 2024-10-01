8dfdcab02e19ec0b87d40c1b53fd68224d1817c8
muhammamdsurya <muhammadsurya2812@gmail.com>
2024-10-01 22:34:06 +0700

    m

--- resources/views/layout/adminLayout.blade.php
+++ resources/views/layout/adminLayout.blade.php
@@ -60,7 +60,7 @@
 
     <!-- jQuery -->
     <!-- Bootstrap JavaScript -->
-    <script src="http://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
